e66fd22fbc4832520a6eb0100c380487634a7962
Philip Fahim <philip.louis49@gmail.com>
2018-01-11 16:29:48 -0500

    feat(Beam Options): Dropdown menu with all beam options to choose from

--- client/components/main.js
+++ client/components/main.js
@@ -15,7 +15,7 @@ const Main = (props) => {
 
   return (
     <div>
-      <h1>BOILERMAKER</h1>
+      <h1>Shear and Moment Diagram</h1>
       <nav>
         {
           isLoggedIn
--- client/components/user-home.js
+++ client/components/user-home.js
@@ -2,22 +2,23 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 
-/**
- * COMPONENT
- */
 export const UserHome = (props) => {
   const {email} = props
 
   return (
     <div>
       <h3>Welcome, {email}</h3>
+      <p>What Shape Would You like to Use?</p>
+      <select>
+        <option>Rectangle</option>
+        <option>I-Beam</option>
+        <option>T-Beam</option>
+        <option>Channel-Beam</option>
+      </select>
     </div>
   )
 }
 
-/**
- * CONTAINER
- */
 const mapState = (state) => {
   return {
     email: state.user.email
@@ -26,9 +27,7 @@ const mapState = (state) => {
 
 export default connect(mapState)(UserHome)
 
-/**
- * PROP TYPES
- */
+// PROP TYPES
 UserHome.propTypes = {
   email: PropTypes.string
 }
