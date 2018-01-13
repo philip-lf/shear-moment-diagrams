ea256d12e4a82bf090926c5ad6825078d8e5fd3f
Philip Fahim <philip.louis49@gmail.com>
2018-01-12 22:16:44 -0500

    feat(beam): persist beam type value in state and pass it to BeamLayout component

--- client/components/user-home.js
+++ client/components/user-home.js
@@ -1,24 +1,41 @@
-import React from 'react'
+import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 import BeamLayout from './BeamLayout'
 
-export const UserHome = (props) => {
-  const {email} = props
+class UserHome extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      beamType: ''
+    }
+    this.handleBeamType = this.handleBeamType.bind(this)
+  }
 
-  return (
-    <div>
-      <h3>Welcome, {email}</h3>
-      <p>What Shape Would You like to Use?</p>
-      <select>
-        <option>Rectangle</option>
-        <option>I-Beam</option>
-        <option>T-Beam</option>
-        <option>Channel-Beam</option>
-      </select>
-      <BeamLayout beamType={'dogcatzebra'} />
-    </div>
-  )
+  handleBeamType(event) {
+    console.log('beam type: ', event.target.value)
+    this.setState({ beamType: event.target.value })
+  }
+
+  render() {
+    const {email} = this.props
+    return (
+      <div>
+        <h3>Welcome, {email}</h3>
+        <p>What Shape Would You like to Use?</p>
+        <select
+          name="beam"
+          value={this.state.beamType}
+          onChange={this.handleBeamType}>
+          <option>Rectangle</option>
+          <option>I-Beam</option>
+          <option>T-Beam</option>
+          <option>Channel-Beam</option>
+        </select>
+        <BeamLayout beamType={this.state.beamType} />
+      </div>
+    )
+  }
 }
 
 const mapState = (state) => {
